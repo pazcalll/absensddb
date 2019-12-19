19b83c1a26a1b8c55f7ca1e1fdb19b587c3e777c
pazcalll <54096103+pazcalll@users.noreply.github.com>
2019-12-19 22:10:09 +0700

    after edit

--- pages/akademik/lapfull2.php
+++ pages/akademik/lapfull2.php
@@ -2,33 +2,43 @@
 <div class="control-group">
 <label class="control-label">Jurusan</label>
 <div class="controls">
+	<select name="jurusan" id="jurusan">
 <?php
-$q=mysql_query("Select * from jurusan");
-while($r=mysql_fetch_array($q))
+
+$host	 = "127.0.0.1";
+$user	 = "root";
+$pass	 = "";
+$dabname = "absensddb";
+
+$foldername="absensddb";
+$conn = mysqli_connect( $host, $user, $pass) or die('Could not connect to mysql server.' );
+mysqli_select_db($conn, $dabname) or die('Could not select database.');
+
+$q=mysqli_query($conn, "Select * from kelas");
+while($r=mysqli_fetch_array($q))
 {
 ?>
-<select name="jurusan" id="jurusan">
-<option value="<?php echo $r['kode_jurusan']; ?>"><?php echo $r['nama_jurusan']; ?></option>
-</select>
+<option value="<?php echo $r['kode_kelas']; ?>"><?php echo $r['nama_kelas']; ?></option>
 <?php
 }
 ?>
+</select>
 </div>
 </div>
 <div class="control-group">
-<label class="control-label">Mata Kuliah</label>
+<label class="control-label">Mata Pelajaran</label>
 <div class="controls">
+	<select name="matkul" id="matkul">
 <?php
-$q=mysql_query("Select * from mata_kuliah");
-while($r=mysql_fetch_array($q))
+$q=mysqli_query($conn, "Select * from mata_pelajaran");
+while($r=mysqli_fetch_array($q))
 {
 ?>
-<select name="matkul" id="matkul">
-<option value="<?php echo $r['kode_mata_kuliah']; ?>"><?php echo $r['nama_mata_kuliah']; ?></option>
-</select>
+<option value="<?php echo $r['kode_mata_pelajaran']; ?>"><?php echo $r['nama_mata_pelajaran']; ?></option>
 <?php
 }
 ?>
+</select>
 </div>
 </div>
 <div class="control-group">
--- pages/akademik/laptengah.php
+++ pages/akademik/laptengah.php
@@ -1,51 +1,62 @@
-<form method="post">
-<div class="control-group">
-<label class="control-label">Jurusan</label>
-<div class="controls">
-<?php
-$q=mysql_query("Select * from jurusan");
-while($r=mysql_fetch_array($q))
-{
-?>
-<select name="jurusan" id="jurusan">
-<option value="<?php echo $r['kode_jurusan']; ?>"><?php echo $r['nama_jurusan']; ?></option>
-</select>
-<?php
-}
-?>
-</div>
-</div>
-<div class="control-group">
-<label class="control-label">Mata Kuliah</label>
-<div class="controls">
-<?php
-$q=mysql_query("Select * from mata_kuliah");
-while($r=mysql_fetch_array($q))
-{
-?>
-<select name="matkul" id="matkul">
-<option value="<?php echo $r['kode_mata_kuliah']; ?>"><?php echo $r['nama_mata_kuliah']; ?></option>
-</select>
-<?php
-}
-?>
-</div>
-</div>
-<div class="control-group">
-<label class="control-label">Semester</label>
-<div class="controls">
-<input type="text" name="semester" id="semester" class="input-small">&nbsp;
-</div>
-</div>
-<div class="control-group">
-<div class="controls">
-<input type="submit" name="simpan" class="btn btn-medium btn-primary" value="Cetak Data" />
-</div>
-</div>
-</form>
-<?php
-if(isset($_POST['simpan']))
-{
-	echo "<script>window.location='".$baseurl."pages/web/lapabsen.php?semester=".$_POST['semester']."&blnadd=3&tipe=MAHASISWA&matkul=".$_POST['matkul']."'</script>";
-}
-?>
+	<form method="post">
+	<div class="control-group">
+	<label class="control-label">Kelas</label>
+	<div class="controls">
+	<select name="jurusan" id="jurusan">
+	<?php
+
+	$host	 = "127.0.0.1";
+		$user	 = "root";
+		$pass	 = "";
+		$dabname = "absensddb";
+		
+		$foldername="absensddb";
+		$conn = mysqli_connect( $host, $user, $pass) or die('Could not connect to mysql server.' );
+		mysqli_select_db($conn, $dabname) or die('Could not select database.');
+
+	$q=mysqli_query($conn, "Select * from Kelas");
+	while($r=mysqli_fetch_array($q))
+	{
+		?>
+		<option value="<?php echo $r['kode_kelas']; ?>"><?php echo $r['nama_kelas']; ?></option>
+
+	<?php
+	}
+	?>
+	</select>
+	</div>
+	</div>
+	<div class="control-group">
+	<label class="control-label">Mata Pelajaran</label>
+	<div class="controls">
+		<select name="matkul" id="matkul">
+	<?php
+	$q=mysqli_query($conn, "Select * from mata_pelajaran");
+	while($r=mysqli_fetch_array($q))
+	{
+	?>
+	<option value="<?php echo $r['kode_mata_pelajaran']; ?>"><?php echo $r['nama_mata_pelajaran']; ?></option>
+	<?php
+	}
+	?>
+	</select>
+	</div>
+	</div>
+	<div class="control-group">
+	<label class="control-label">Semester</label>
+	<div class="controls">
+	<input type="text" name="semester" id="semester" class="input-small">&nbsp;
+	</div>
+	</div>
+	<div class="control-group">
+	<div class="controls">
+	<input type="submit" name="simpan" class="btn btn-medium btn-primary" value="Cetak Data" />
+	</div>
+	</div>
+	</form>
+	<?php
+	if(isset($_POST['simpan']))
+	{
+		echo "<script>window.location='".$baseurl."pages/web/lapabsen.php?semester=".$_POST['semester']."&blnadd=3&tipe=MAHASISWA&matkul=".$_POST['matkul']."'</script>";
+	}
+	?>
